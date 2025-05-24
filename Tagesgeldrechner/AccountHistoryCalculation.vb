7787ef08ca74767eb71708d8e978ca13ae1e8fcc
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-05-24 10:14:33 +0200

    Verbessere Dokumentation und Kommentare in `AccountHistoryCalculation.vb`
    Die Änderungen umfassen eine umfassende Überarbeitung der Dokumentation und des Codes der Funktion `Calculate_Verlauf()`. Detailliertere Kommentare wurden hinzugefügt, um den Zweck der Funktion und die Berechnung der Zinsen klarer zu erklären. Die Struktur des Codes bleibt unverändert, jedoch wurden die Kommentare aktualisiert, um die Lesbarkeit und das Verständnis zu verbessern. Die Funktionalität bleibt gleich, aber die Erklärungen sind jetzt präziser und benutzerfreundlicher.

--- Tagesgeldrechner/AccountHistoryCalculation.vb
+++ Tagesgeldrechner/AccountHistoryCalculation.vb
@@ -4,52 +4,55 @@
 ' Copyright (c)2025 by Andreas Sauer 
 '
 ' Kurzbeschreibung:
+' Berechnet den Verlauf eines Tagesgeldkontos über eine bestimmte Laufzeit unter Berücksichtigung
+' von regelmäßigen Einzahlungen und verschiedenen Zinszahlungsintervallen.
 ' 
 ' *************************************************************************************************
 
 Module AccountHistoryCalculation
 
-  ''' <summary>
-  ''' Berechnet den Kontoverlauf
-  ''' </summary>
-  Friend Function Calculate_Verlauf() As List(Of KontoInfo)
-
-    Dim result As New List(Of KontoInfo)
-    result.Clear()
-
-    Dim Zins As Double = 0
-    Dim Kontostand As Double = StartSaldo
-
-    For Monat As Double = 1 To Laufzeit
-
-      Kontostand += Zahlung
-
-      Select Case ZinsTurnus
-
-        Case 0 'monatliche Zinszahlung
-          Zins = Math.Round(Kontostand * ZinsFaktor, 2)
-          Kontostand += Zins
-
-        Case 1 'virteljährliche Zinszahlung
-          Zins = If(Monat Mod 3 = 0, Math.Round(Kontostand * ZinsFaktor, 2), 0)
-          Kontostand += Zins
-
-        Case 2 'halbjährliche Zinszahlung
-          Zins = If(Monat Mod 6 = 0, Math.Round(Kontostand * ZinsFaktor, 2), 0)
-          Kontostand += Zins
-
-        Case 3 'jährliche Zinszahlung
-          Zins = If(Monat Mod 12 = 0, Math.Round(Kontostand * ZinsFaktor, 2), 0)
-          Kontostand += Zins
-
-      End Select
-
-      result.Add(New KontoInfo With {.Monat = Monat, .Kontostand = Kontostand, .Zinsen = Zins})
-
-    Next
-
-    Return result
-
-  End Function
+    ''' <summary>
+    ''' Berechnet den Kontoverlauf für ein Tagesgeldkonto.
+    ''' Für jeden Monat der Laufzeit wird der aktuelle Kontostand und die erhaltenen Zinsen berechnet.
+    ''' Die Zinsgutschrift erfolgt je nach gewähltem Zinsintervall (monatlich, vierteljährlich, halbjährlich, jährlich).
+    ''' </summary>
+    ''' <returns>Liste mit KontoInfo-Objekten für jeden Monat</returns>
+    Friend Function Calculate_Verlauf() As List(Of KontoInfo)
+        ' Ergebnisliste für den Verlauf initialisieren
+        Dim result As New List(Of KontoInfo)
+        result.Clear()
+        ' Variable für die Zinsen des aktuellen Monats
+        Dim Zins As Double = 0
+        ' Startwert für den Kontostand setzen
+        Dim Kontostand As Double = StartSaldo
+        ' Für jeden Monat der Laufzeit:
+        For Monat As Double = 1 To Laufzeit
+            ' Monatliche Einzahlung zum Kontostand addieren
+            Kontostand += Zahlung
+            ' Je nach ZinsTurnus wird die Zinsgutschrift unterschiedlich berechnet
+            Select Case ZinsTurnus
+                Case 0 ' monatliche Zinszahlung
+                    ' Zinsen werden jeden Monat berechnet und gutgeschrieben
+                    Zins = Math.Round(Kontostand * ZinsFaktor, 2)
+                    Kontostand += Zins
+                Case 1 ' vierteljährliche Zinszahlung
+                    ' Zinsen werden nur alle 3 Monate berechnet und gutgeschrieben
+                    Zins = If(Monat Mod 3 = 0, Math.Round(Kontostand * ZinsFaktor, 2), 0)
+                    Kontostand += Zins
+                Case 2 ' halbjährliche Zinszahlung
+                    ' Zinsen werden nur alle 6 Monate berechnet und gutgeschrieben
+                    Zins = If(Monat Mod 6 = 0, Math.Round(Kontostand * ZinsFaktor, 2), 0)
+                    Kontostand += Zins
+                Case 3 ' jährliche Zinszahlung
+                    ' Zinsen werden nur alle 12 Monate berechnet und gutgeschrieben
+                    Zins = If(Monat Mod 12 = 0, Math.Round(Kontostand * ZinsFaktor, 2), 0)
+                    Kontostand += Zins
+            End Select
+            ' Für den aktuellen Monat ein KontoInfo-Objekt mit Monat, Kontostand und Zinsen zur Ergebnisliste hinzufügen
+            result.Add(New KontoInfo With {.Monat = Monat, .Kontostand = Kontostand, .Zinsen = Zins})
+        Next
+        ' Die vollständige Verlaufsliste zurückgeben
+        Return result
+    End Function
 
 End Module

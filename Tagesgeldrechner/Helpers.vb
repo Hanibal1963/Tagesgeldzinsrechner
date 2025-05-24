013ed8ef508aac586ffc2ac6383be113995a57a2
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-05-24 10:18:54 +0200

    Verbessere Kommentare in der Methode SetVariables
    Die Methode `SetVariables` wurde aktualisiert, um die Formatierung der Kommentare zu verbessern. Es wurden keine funktionalen Änderungen am Code selbst vorgenommen; lediglich die Lesbarkeit der Dokumentation wurde optimiert.

--- Tagesgeldrechner/Helpers.vb
+++ Tagesgeldrechner/Helpers.vb
@@ -9,30 +9,23 @@
 
 Module Helpers
 
-  ''' <summary>
-  ''' Variablen je nach Textbox setzen
-  ''' </summary>
-  ''' <param name="sender"></param>
-  ''' <param name="value"></param>
-  Friend Sub SetVariables(sender As TextBox, value As Double)
-
-    'Der Inhalt welcher Textbox hat sich geändert
-    Select Case sender.Name
-
-      Case $"TextBoxAnfangssaldo" 'Der Anfangssaldo hat sich geändert
-        StartSaldo = value
-
-      Case $"TextBoxEinzahlung" 'Die monatliche Einzahlung hat sich geändert
-        Zahlung = value
-
-      Case $"TextBoxLaufzeit" 'Die Laufzeit hat sich geändert
-        Laufzeit = value
-
-      Case $"TextBoxZinssatz" 'Der Zinssatz hat sich geändert
-        ZinsPa = value
-
-    End Select
-
-  End Sub
+    ''' <summary>
+    ''' Variablen je nach Textbox setzen
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="value"></param>
+    Friend Sub SetVariables(sender As TextBox, value As Double)
+        'Der Inhalt welcher Textbox hat sich geändert
+        Select Case sender.Name
+            Case $"TextBoxAnfangssaldo" 'Der Anfangssaldo hat sich geändert
+                StartSaldo = value
+            Case $"TextBoxEinzahlung" 'Die monatliche Einzahlung hat sich geändert
+                Zahlung = value
+            Case $"TextBoxLaufzeit" 'Die Laufzeit hat sich geändert
+                Laufzeit = value
+            Case $"TextBoxZinssatz" 'Der Zinssatz hat sich geändert
+                ZinsPa = value
+        End Select
+    End Sub
 
 End Module

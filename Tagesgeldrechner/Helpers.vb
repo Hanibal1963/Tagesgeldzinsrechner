f0fa8afe7d52a99f8af19a2e7639b0d9f4d359e4
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2025-05-24 10:21:34 +0200

    Verbessere Kommentare in Helpers.vb
    Aktualisiere die Beschreibungen der Methoden und Parameter, insbesondere für `SetVariables`, um die Funktionsweise klarer zu machen. Erweitere die Kommentare im `Select Case`-Block, um die Zuordnung der Variablen zu den geänderten TextBoxen deutlicher zu erläutern.

--- Tagesgeldrechner/Helpers.vb
+++ Tagesgeldrechner/Helpers.vb
@@ -4,27 +4,28 @@
 ' Copyright (c)2025 by Andreas Sauer 
 '
 ' Kurzbeschreibung:
+' Hilfsmodul mit Methoden zur Variablenzuweisung anhand von TextBox-Eingaben.
 ' 
 ' *************************************************************************************************
 
 Module Helpers
 
     ''' <summary>
-    ''' Variablen je nach Textbox setzen
+    ''' Setzt globale Variablen abhängig davon, welche TextBox geändert wurde.
     ''' </summary>
-    ''' <param name="sender"></param>
-    ''' <param name="value"></param>
+    ''' <param name="sender">Die TextBox, deren Wert geändert wurde.</param>
+    ''' <param name="value">Der neue Wert, der gesetzt werden soll.</param>
     Friend Sub SetVariables(sender As TextBox, value As Double)
-        'Der Inhalt welcher Textbox hat sich geändert
+        ' Überprüft, welche TextBox den Wert geändert hat und weist den Wert der entsprechenden Variable zu.
         Select Case sender.Name
-            Case $"TextBoxAnfangssaldo" 'Der Anfangssaldo hat sich geändert
-                StartSaldo = value
-            Case $"TextBoxEinzahlung" 'Die monatliche Einzahlung hat sich geändert
-                Zahlung = value
-            Case $"TextBoxLaufzeit" 'Die Laufzeit hat sich geändert
-                Laufzeit = value
-            Case $"TextBoxZinssatz" 'Der Zinssatz hat sich geändert
-                ZinsPa = value
+            Case $"TextBoxAnfangssaldo" ' Der Anfangssaldo wurde geändert
+                StartSaldo = value      ' Setzt den Startsaldo
+            Case $"TextBoxEinzahlung"   ' Die monatliche Einzahlung wurde geändert
+                Zahlung = value         ' Setzt die monatliche Einzahlung
+            Case $"TextBoxLaufzeit"     ' Die Laufzeit wurde geändert
+                Laufzeit = value        ' Setzt die Laufzeit
+            Case $"TextBoxZinssatz"     ' Der Zinssatz wurde geändert
+                ZinsPa = value          ' Setzt den Zinssatz pro Jahr
         End Select
     End Sub
 

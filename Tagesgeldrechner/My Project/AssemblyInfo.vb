8b31a782100ea5d619117639f4845328eb0204bc
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2026-03-29 14:24:32 +0200

    Assembly-Info: Copyright & Versionen aktualisiert
    - Copyright-Jahr auf „2025 - 2026 by Andreas Sauer“ geändert.
    - AssemblyVersion und AssemblyFileVersion auf 1.2026.0329.0 erhöht.
    - Attributname für AssemblyInformationalVersion korrigiert.

--- Tagesgeldrechner/My Project/AssemblyInfo.vb
+++ Tagesgeldrechner/My Project/AssemblyInfo.vb
@@ -2,34 +2,15 @@ Imports System.Reflection
 Imports System.Resources
 Imports System.Runtime.InteropServices
 
-' Allgemeine Informationen über eine Assembly werden über die folgenden
-' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-' die einer Assembly zugeordnet sind.
-
-' Werte der Assemblyattribute überprüfen
-
 <Assembly: AssemblyTitle("Tagesgeldrechner")>
 <Assembly: AssemblyDescription("Tool zum Berechnen des Kontoverlaufs eines Tagesgeldkontos auf Basis der Benutzerangaben.")>
 <Assembly: AssemblyCompany("Andreas Sauer")>
 <Assembly: AssemblyProduct("Tagesgeldrechner")>
-<Assembly: AssemblyCopyright("© 2025 Andreas Sauer")>
+<Assembly: AssemblyCopyright("© 2025 - 2026 by Andreas Sauer")>
 <Assembly: AssemblyTrademark("SchlumpfSoft")>
-
 <Assembly: ComVisible(False)>
-
-'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
 <Assembly: Guid("a3bbc338-a925-4aaf-877d-1abf541e7902")>
-
-' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-'
-'      Hauptversion
-'      Nebenversion
-'      Buildnummer
-'      Revision
-'
-
-<Assembly: AssemblyVersion("1.2025.0804.0")>
-<Assembly: AssemblyFileVersion("1.2025.0804.0")>
+<Assembly: AssemblyVersion("1.2026.0329.0")>
+<Assembly: AssemblyFileVersion("1.2026.0329.0")>
 <Assembly: NeutralResourcesLanguage("de-DE")>
-
-<assembly: AssemblyInformationalVersion("0.2025.0525.1")>
+<Assembly: AssemblyInformationalVersion("0.2025.0525.1")>
